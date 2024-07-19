590c399f18afdecab212726dec9df935a45bc0ab
qx6ghqkz <93668667+qx6ghqkz@users.noreply.github.com>
2024-07-19 16:14:22 +0100

    feat: manually merge changes from test branch

--- Dockerfile
+++ Dockerfile
@@ -8,23 +8,44 @@ RUN apk add --no-cache \
   ffmpeg \
   tzdata
 
-RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
 
+RUN python -m venv venv
+
+ENV PATH="/usr/src/app/venv/bin:$PATH"
+
 COPY requirements.txt .
-RUN apk --update-cache add --virtual build-dependencies gcc libc-dev make \
+
+RUN apk add --no-cache --virtual build-dependencies gcc libc-dev make \
   && pip install --no-cache-dir --upgrade pip \
   && pip install --no-cache-dir -r requirements.txt \
   && apk del build-dependencies
 
 COPY . .
 
-ENV CONTAINER_PORT 9080
+ENV CONTAINER_PORT=9080
 
-EXPOSE ${CONTAINER_PORT}
+EXPOSE $CONTAINER_PORT
 
 VOLUME ["/gallery-dl"]
 
+ENV USER=app
+ENV GROUP=$USER
+ENV UID=1000
+ENV GID=$UID
+
+RUN addgroup --gid "$GID" "$GROUP" \
+  && adduser --disabled-password --gecos "" --home "$(pwd)" --ingroup "$GROUP" --no-create-home --uid "$UID" $USER
+
+RUN mkdir -p /.cache/pip \
+  && mkdir /.local
+
+RUN chown -R $USER:$GROUP . \
+  && chown -R $USER:$GROUP /.cache/pip \
+  && chown -R $USER:$GROUP /.local
+
 RUN chmod +x ./start.sh
+
+USER $USER
 
 CMD ["./start.sh"]
--- gallery-dl-server.py
+++ gallery-dl-server.py
@@ -34,18 +34,21 @@ async def q_put(request):
     form = await request.form()
     url = form.get("url").strip()
     ui = form.get("ui")
+    options = {"format": form.get("format")}
 
     if not url:
         return JSONResponse(
             {"success": False, "error": "/q called without a 'url' in form data"}
         )
 
-    task = BackgroundTask(download, url)
+    task = BackgroundTask(download, url, options)
 
     print("Added url " + url + " to the download queue")
 
     if not ui:
-        return JSONResponse({"success": True, "url": url}, background=task)
+        return JSONResponse(
+            {"success": True, "url": url, "options": options}, background=task
+        )
     return RedirectResponse(
         url="/gallery-dl?added=" + url, status_code=HTTP_303_SEE_OTHER, background=task
     )
@@ -58,14 +61,6 @@ async def update_route(scope, receive, send):
 
 
 def update():
-    try:
-        output = subprocess.check_output(
-            [sys.executable, "-m", "pip", "install", "--upgrade", "pip"]
-        )
-
-        print(output.decode("utf-8"))
-    except subprocess.CalledProcessError as e:
-        print(e.output)
     try:
         output = subprocess.check_output(
             [sys.executable, "-m", "pip", "install", "--upgrade", "gallery_dl"]
@@ -84,9 +79,116 @@ def update():
         print(e.output)
 
 
-def download(url):
+def config_remove(path, key=None, value=None):
+    entries = []
+    removed_entries = []
+
+    if isinstance(path, list):
+        _list = path
+
+        for entry in _list:
+            if key:
+                if value:
+                    if entry.get(key) == value:
+                        entries.append(entry)
+                else:
+                    if entry.get(key):
+                        entries.append(entry)
+            else:
+                if entry == value:
+                    entries.append(entry)
+
+        for entry in entries:
+            try:
+                _list.remove(entry)
+            except Exception as e:
+                print("Exception: " + str(e))
+            else:
+                removed_entries.append(entry)
+
+    if isinstance(path, dict):
+        _dict = path
+
+        if value:
+            for k, v in _dict.items():
+                if k == key and v == value:
+                    entries.append(k)
+        else:
+            for k in _dict.keys():
+                if k == key:
+                    entries.append(k)
+
+        for entry in entries:
+            try:
+                _dict.pop(entry)
+            except Exception as e:
+                print("Exception: " + str(e))
+            else:
+                removed_entries.append(entry)
+
+    return removed_entries
+
+
+def config_update(request_options):
+    requested_format = request_options.get("format", "select")
+
+    if requested_format == "video":
+        try:
+            cmdline_args = (
+                config._config.get("extractor", {})
+                .get("ytdl", {})
+                .get("cmdline-args", [])
+            )
+        except AttributeError:
+            pass
+        else:
+            config_remove(cmdline_args, None, "--extract-audio")
+            config_remove(cmdline_args, None, "-x")
+
+        try:
+            raw_options = (
+                config._config.get("extractor", {})
+                .get("ytdl", {})
+                .get("raw-options", {})
+            )
+        except AttributeError:
+            pass
+        else:
+            config_remove(raw_options, "writethumbnail", False)
+
+        try:
+            postprocessors = (
+                config._config.get("extractor", {})
+                .get("ytdl", {})
+                .get("raw-options", {})
+                .get("postprocessors", [])
+            )
+        except AttributeError:
+            pass
+        else:
+            config_remove(postprocessors, "key", "FFmpegExtractAudio")
+
+    if requested_format == "audio":
+        config.set(
+            ("extractor", "ytdl"),
+            "raw-options",
+            {
+                "writethumbnail": False,
+                "postprocessors": [
+                    {
+                        "key": "FFmpegExtractAudio",
+                        "preferredcodec": "best",
+                        "preferredquality": 320,
+                    }
+                ],
+            },
+        )
+
+
+def download(url, request_options):
     config.clear()
     config.load()
+    config_update(request_options)
     job.DownloadJob(url).run()
 
 
@@ -100,5 +202,5 @@ routes = [
 
 app = Starlette(debug=True, routes=routes)
 
-print("Updating gallery-dl and yt-dlp to the latest version . . . ")
+print("\nUpdating gallery-dl and yt-dlp to the latest version . . . \n")
 update()
