329648213ca177fe3ee12a94cf83707f467b8e99
qx6ghqkz <93668667+qx6ghqkz@users.noreply.github.com>
2025-02-12 03:20:39 +0000

    feat: add button to clear logs

--- gallery_dl_server/static/js/logs.js
+++ gallery_dl_server/static/js/logs.js
@@ -1,4 +1,30 @@
+const logContainer = document.getElementById("container-logs");
+const clearLogsButton = document.getElementById("clear-logs");
+
 window.onload = () => {
-  const logContainer = document.getElementById("logContainer");
   logContainer.scrollTop = logContainer.scrollHeight;
+};
+
+clearLogsButton.onclick = () => {
+  clearLogsButton.disabled = true;
+
+  fetch("/gallery-dl/logs/clear", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network response was not OK.");
+    })
+    .then((data) => {
+      logContainer.innerText = "Cleared logs.";
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      logContainer.innerText = "Failed to clear logs.";
+    });
 };
