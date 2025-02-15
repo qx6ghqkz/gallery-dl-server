6530fad0fa4cacd509c412dc5bc93af9c5a54df3
qx6ghqkz <93668667+qx6ghqkz@users.noreply.github.com>
2025-02-15 06:34:09 +0000

    feat: add option to set log file directory
    Changes:
    - Add command-line argument `--log-dir` for setting the log file directory
    - Handle `TypeError` when parsing command-line arguments
    - Show WebSocket connection logs based on access log setting instead of log level
    - Set the log level with the same command-line argument used to set Uvicorn's log level
    - Move server logic into `server.py` and manage `__init__.py` to avoid circular imports and ensure the server is not initialised before command-line arguments are parsed
    - Add Python version check in `__init__.py` and raise `ImportError` if Python version is not 3.10 or above
    - Take the first steps towards defining a public API: add ability to run the package programatically via `run()` command in `__init__.py` on package import
    - Ensure command-line arguments can only be parsed once and assign the parsed arguments to a global variable so they can be accessed from other modules

--- gallery_dl_server/__main__.py
+++ gallery_dl_server/__main__.py
@@ -1,35 +1,46 @@
 # -*- coding: utf-8 -*-
 
+import sys
 import multiprocessing
 
 import uvicorn
 
-from gallery_dl_server import output, options, app
+from gallery_dl_server import options
 
 
-log = output.initialise_logging(__name__)
+def main(args: options.CustomNamespace | None = None):
+    """Main entry point for running gallery-dl-server."""
+    if args is None:
+        try:
+            args = options.parse_args(__name__)
+        except TypeError as e:
+            sys.stderr.write(f"{type(e).__name__}: {e}\n")
+            sys.exit(1)
 
+    from gallery_dl_server import output, server
 
-def main():
+    log = output.initialise_logging(__name__)
+
     multiprocessing.freeze_support()
 
-    args = options.parse_args(__name__)
-
     kwargs = {
-        "app": app,
+        "app": server.app,
         "host": args.host,
         "port": args.port,
         "log_level": args.log_level,
         "access_log": args.access_log,
     }
 
-    config = uvicorn.Config(**kwargs)
-    server = uvicorn.Server(config)
+    uvicorn_config = uvicorn.Config(**kwargs)
+    uvicorn_server = uvicorn.Server(uvicorn_config)
 
     try:
-        server.run()
+        uvicorn_server.run()
     except KeyboardInterrupt as e:
         log.debug(f"Exception: {type(e).__name__}")
+    except Exception as e:
+        log.error(f"An unexpected error occurred: {type(e).__name__}: {e}")
+        sys.exit(1)
 
 
 if __name__ == "__main__":
