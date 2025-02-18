b4158b1686f674979be59ef724f2e20764ad95bb
qx6ghqkz <93668667+qx6ghqkz@users.noreply.github.com>
2025-02-18 01:23:34 +0000

    refactor: move startup logic into a separate module for import
    Changes:
    - Moved startup logic from `__main__.py` into `app.py` to avoid importing `__main__` in the `__init__` module and receiving the following warning when importing `__main__` globally and running the package directly with Python: `RuntimeWarning: 'gallery_dl_server.__main__' found in sys.modules after import of package 'gallery_dl_server', but prior to execution of 'gallery_dl_server.__main__'; this may result in unpredictable behaviour`

--- gallery_dl_server/__main__.py
+++ gallery_dl_server/__main__.py
@@ -1,50 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import os
-import sys
-import multiprocessing
-
-import uvicorn
-
-from gallery_dl_server import options
-
-
-def main(args: options.CustomNamespace | None = None):
-    """Main entry point for gallery-dl-server."""
-    if args is None:
-        try:
-            args = options.parse_args(__name__)
-        except TypeError as e:
-            sys.stderr.write(f"{type(e).__name__}: {e}\n")
-            sys.exit(1)
-
-    from gallery_dl_server import output, server
-
-    log = output.initialise_logging(__name__)
-
-    multiprocessing.freeze_support()
-
-    kwargs = {
-        "app": server.app,
-        "host": args.host,
-        "port": args.port,
-        "log_level": args.log_level,
-        "access_log": args.access_log,
-        "proxy_headers": os.environ.get("PROXY_HEADERS", "true").lower() == "true",
-        "forwarded_allow_ips": os.environ.get("FORWARDED_ALLOW_IPS", "*"),
-    }
-
-    uvicorn_config = uvicorn.Config(**kwargs)
-    uvicorn_server = uvicorn.Server(uvicorn_config)
-
-    try:
-        uvicorn_server.run()
-    except KeyboardInterrupt as e:
-        log.debug(f"Exception: {type(e).__name__}")
-    except Exception as e:
-        log.error(f"An unexpected error occurred: {type(e).__name__}: {e}")
-        sys.exit(1)
-
+from gallery_dl_server import app
 
 if __name__ == "__main__":
-    main()
+    app.main()
