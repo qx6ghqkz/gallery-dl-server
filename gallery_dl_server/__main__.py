c57cbdac72bbb7a77c2a3db4d06191d690733b42
qx6ghqkz <93668667+qx6ghqkz@users.noreply.github.com>
2025-03-09 23:52:38 +0000

    fix: standalone executable multiprocessing support
    - Move multiprocessing freeze support invocation back to the main module to fix the Windows executable

--- gallery_dl_server/__main__.py
+++ gallery_dl_server/__main__.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
+import multiprocessing
+
 from gallery_dl_server import app
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     app.main(module_name=__name__)
